e411d945d1d21e3d194c69a3ab1c81e83a00d0b0
bobertfishbone <bobertfishbone@gmail.com>
2015-07-18 09:46:26 -0400

    Werx v1.0
    v1.0

--- honeybad/honeybad.ino
+++ honeybad/honeybad.ino
@@ -24,7 +24,7 @@
 
 int numSteps = 0;
 int hours = 0;
- int minutes = 0;
+int minutes = 0;
 
 int myTemp[10];
 long stageDuration[10];
@@ -35,99 +35,106 @@ long stageStart;
 LiquidCrystal lcd(RS, enable, D4, D5, D6, D7);
 
 void setup() {
-  lcd.begin(16,2);
-Serial.begin(9600);
+  lcd.begin(16, 2);
+  Serial.begin(9600);
 
-pinMode(homebutton, OUTPUT);
-pinMode(menubutton, OUTPUT);
-pinMode(leftbutton, OUTPUT);
-pinMode(rightbutton, OUTPUT);
-pinMode(upbutton, OUTPUT);
-pinMode(downbutton, OUTPUT);
+  pinMode(homebutton, OUTPUT);
+  pinMode(menubutton, OUTPUT);
+  pinMode(leftbutton, OUTPUT);
+  pinMode(rightbutton, OUTPUT);
+  pinMode(upbutton, OUTPUT);
+  pinMode(downbutton, OUTPUT);
 
-pinMode(buttonup, INPUT_PULLUP);
-pinMode(buttondown, INPUT_PULLUP);
-pinMode(buttonselect, INPUT_PULLUP);
+  pinMode(buttonup, INPUT_PULLUP);
+  pinMode(buttondown, INPUT_PULLUP);
+  pinMode(buttonselect, INPUT_PULLUP);
 
-digitalWrite(homebutton, LOW);
-digitalWrite(menubutton, LOW);
-digitalWrite(leftbutton, LOW);
-digitalWrite(rightbutton, LOW);
-digitalWrite(upbutton, LOW);
-digitalWrite(downbutton, LOW);
-
-lcd.clear();
-lcd.setCursor(0,0);
-lcd.print("How many stages?");
-numSteps = getNumber(1);
-
-for (int i = 1; i <= numSteps; i++) {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Setpoint for stg ");
-  lcd.print(i);
-  myTemp[i] = getNumber(3);
+  digitalWrite(homebutton, LOW);
+  digitalWrite(menubutton, LOW);
+  digitalWrite(leftbutton, LOW);
+  digitalWrite(rightbutton, LOW);
+  digitalWrite(upbutton, LOW);
+  digitalWrite(downbutton, LOW);
 
   lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Duration (mins) for stg ");
-  lcd.print(i);
-  stageDuration[i] = getNumber(3) * 20000;
-}
+  lcd.setCursor(0, 0);
+  lcd.print("How many stages?");
+  numSteps = getNumber(1);
+  //numSteps = 5;
 
-startTime = millis();
+  for (int i = 1; i <= numSteps; i++) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Setpoint for stg ");
+    lcd.print(i);
+    myTemp[i] = getNumber(3);
+    //myTemp[i] = 2*i;
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Duration (mins) for stg ");
+    lcd.print(i);
+    stageDuration[i] = getNumber(3) * 20000;
+    //stageDuration[i] = 20000;
+  }
+
+  startTime = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-for (int i = 1; i <= numSteps; i++) {
-  stageStart = millis();
-  Serial.print("Starting stage ");
-  Serial.println(i);
-  Serial.print("Temp ");
-  Serial.print(myTemp[i]);
-  Serial.print("   Duration ");
-  Serial.println(stageDuration[i]);
-  
-  if (i > 1) {
+  for (int i = 1; i <= numSteps; i++) {
+    
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Starting stage ");
+    lcd.print(i);
+    lcd.setCursor(0,1);
+    lcd.print("Temp ");
+    lcd.print(myTemp[i]);
+    Serial.print("   Duration ");
+    Serial.println(stageDuration[i]);
+
+    if (i > 1) {
       lcd.clear();
-  lcd.setCursor(0,0);
-    lcd.print("Changing by ");
-    lcd.print(myTemp[i] - myTemp[i-1]);
-    changeSetPoint(myTemp[i] - myTemp[i-1]);
+      lcd.setCursor(0, 0);
+      lcd.print("Changing by ");
+      lcd.print(myTemp[i] - myTemp[i - 1]);
+      changeSetPoint(myTemp[i] - myTemp[i - 1]);
     }
-
-  while ((millis() - stageStart) < stageDuration[i])
-  {
-    bool spaceHog = true;
+    stageStart = millis();
+    while ((millis() - stageStart) < stageDuration[i])
+    {
+      bool spaceHog = true;
+    }
   }
-}
-digitalWrite(homebutton, HIGH);
-digitalWrite(menubutton, HIGH);
-digitalWrite(leftbutton, HIGH);
-digitalWrite(rightbutton, HIGH);
-digitalWrite(upbutton, HIGH);
-digitalWrite(downbutton, HIGH);
-delay(9999999999999999999999);
-  lcd.clear();
-  lcd.setCursor(0,0);
+  digitalWrite(homebutton, HIGH);
+  digitalWrite(menubutton, HIGH);
+  digitalWrite(leftbutton, HIGH);
+  digitalWrite(rightbutton, HIGH);
+  digitalWrite(upbutton, HIGH);
+  digitalWrite(downbutton, HIGH);
+    lcd.clear();
+  lcd.setCursor(0, 0);
   lcd.print("DONE");
+  delay(9999999999999999999999);
+
 }
 
-void changeSetPoint(int degrees){
+void changeSetPoint(int degrees) {
   Serial.println("Pushing menu");
   pushButton(menubutton, 1);
   Serial.println("Pushing right");
-    pushButton(rightbutton, 1);
-    
-    if (degrees > 0){
-      Serial.println("Pushing up");
-  pushButton(upbutton, degrees);
-    }
-    else {
-      Serial.println("Pushing down");
-      pushButton(downbutton, degrees*-1);
-      }
+  pushButton(rightbutton, 1);
+
+  if (degrees > 0) {
+    Serial.println("Pushing up");
+    pushButton(upbutton, degrees);
+  }
+  else {
+    Serial.println("Pushing down");
+    pushButton(downbutton, degrees * -1);
+  }
   goHome();
 }
 
@@ -136,26 +143,30 @@ void goHome() {
   pushButton(homebutton, 1);
 }
 
-void pushButton(int button, int times){
-  for (int i = 1; i<=times; i++){
-  long time1 = millis();
-  Serial.println("PUSH!");
-  while(millis() < time1+250){
-  digitalWrite(button, HIGH);
-  
-  }
-  while (millis() < time1+500){
-    digitalWrite(button, LOW);
-  }
+void pushButton(int button, int times) {
+  for (int i = 1; i <= times; i++) {
+    long time1 = millis();
+    Serial.println("PUSH!");
+    while (millis() < time1 + 250) {
+      digitalWrite(button, HIGH);
+
+    }
+    while (millis() < time1 + 500) {
+      digitalWrite(button, LOW);
+    }
   }
 }
 
-int getNumber(int digits) {
-  int digit[3] = {0, 0, 0};
-  lcd.setCursor(0,0);
-  for (int i = digits; i >= 1; i--)
+int getNumber(unsigned int digits) {
+  int digit[4] = {0, 0, 0, 0};
+
+  lcd.setCursor(0,1);
+  lcd.print("000");
+
+  for (unsigned int i = digits; i >= 1; i--)
   {
-    
+
+
     while (digitalRead(buttonselect) == HIGH) {
       //Serial.println("BUTTONSELECT IS HIGH");
       if (digitalRead(buttonup) == LOW) {
@@ -167,7 +178,18 @@ int getNumber(int digits) {
         else if (digit[i] > 9) {
           digit[i] = 0;
         }
-        lcd.setCursor(i, 1);
+        switch(i) {
+          case 3:
+          lcd.setCursor(0, 1);
+          break;
+          case 2:
+          lcd.setCursor(1, 1);
+          break;
+          case 1:
+          lcd.setCursor(2, 1);
+          break;
+        }
+        Serial.println(digit[i]);
         lcd.print(digit[i]);
         delay(250);
       }
@@ -179,11 +201,17 @@ int getNumber(int digits) {
     }
     delay(250);
   }
-  Serial.println(digit[1]);
+  /*Serial.println(digit[1]);
   Serial.println(digit[2]);
   Serial.println(digit[3]);
-  
-  int returnval = (digit[1]*100) + (digit[2] * 10) + digit[3];
+  */
+  Serial.print("3: ");
+  Serial.println(digit[3]);
+  Serial.print("2: ");
+  Serial.println(digit[2]);
+  Serial.print("1: ");
+  Serial.println(digit[1]);
+  unsigned int returnval = (digit[3] * 100) + (digit[2] * 10) + digit[1];
   //Serial.print("RETURNIN!!! ");
   Serial.println(returnval);
   return returnval;
